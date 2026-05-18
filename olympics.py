be389d8488e510484b229ae6f6391e3efb23f75c
arthur pedreira <arthurcp2@al.insper.edu.br>
2026-05-18 16:51:06 -0300

    Update olympics.py

--- olympics.py
+++ olympics.py
@@ -1,1 +1,10 @@
+import pygame
+import sys
+import math
+import os
 
+pygame.init()
+
+WIDTH, HEIGHT = 1000, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("1V1 OLYMPICS")
